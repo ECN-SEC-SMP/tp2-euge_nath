00b18d9e45d1699a454e0aa99cf6f7bb6113d396
Nandros <nathan.andre.etudiant@gmail.com>
2025-10-14 11:17:50 +0200

    MAIN Test for Vehicule::mettreEnPanne()

--- main.cpp
+++ main.cpp
@@ -1,4 +1,17 @@
+#include <cstdint>
+#include <time.h>
+#include <random>
+#include <iostream>
 
+// #include "Vehicule.hpp"
+
+namespace util
+{
+    uint32_t getRandInt(uint32_t min, uint32_t max);
+} // namespace util
+
+
+void setVehiculePanne(Vehicule v);
 
 /**
  * @brief 
@@ -9,13 +22,26 @@
  */
 int main(int argc, char const *argv[])
 {
-    /* code */
+    std::srand(time(NULL));  // Init random seed
+
     return 0;
+}
+
+uint32_t util::getRandInt(uint32_t min, uint32_t max) {
+    return min + ( std::rand() % ( max - min + 1 ) );
 }
 
 // --------------------------------------------------
 // 1 - Creation véhicule
 // --------------------------------------------------
+
+void setVehiculePanne(Vehicule v) {
+    double rNum = std::rand() / (double)RAND_MAX; // Get random number between 0 and 1
+    // std::cout << "Rand = " << rNum << "\n";
+
+    v.mettreEnPanne(rNum);
+
+}
 
 // --------------------------------------------------
 // 2 - Voiture et Bateau
